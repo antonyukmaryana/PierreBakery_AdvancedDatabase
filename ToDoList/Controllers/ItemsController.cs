30205ddb5d885bfa480d577104ff0ee3b0547b71
Student <example@example.com>
2019-08-12 14:27:18 -0700

    Add working version of ManytoMany

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -17,10 +17,9 @@ namespace ToDoList.Controllers
     }
 
     public ActionResult Index()
-    {
-      List<Item> model = _db.Items.Include(items => items.Category).ToList();
-      return View(model);
-    }
+{
+    return View(_db.Items.ToList());
+}
 
     public ActionResult Create()
     {
@@ -28,48 +27,85 @@ namespace ToDoList.Controllers
       return View();
     }
 
-    [HttpPost]
-    public ActionResult Create(Item item)
+[HttpPost]
+public ActionResult Create(Item item, int CategoryId)
+{
+    _db.Items.Add(item);
+    if (CategoryId != 0)
     {
-      _db.Items.Add(item);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
     }
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+}
 
     public ActionResult Details(int id)
-    {
-      Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-      return View(thisItem);
-    }
+{
+    var thisItem = _db.Items
+        .Include(item => item.Categories)
+        .ThenInclude(join => join.Category)
+        .FirstOrDefault(item => item.ItemId == id);
+    return View(thisItem);
+}
 
-    public ActionResult Edit(int id)
-    {
-      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-      return View(thisItem);
-    }
+public ActionResult Edit(int id)
+{
+    var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    return View(thisItem);
+}
 
-    [HttpPost]
-    public ActionResult Edit(Item item)
+[HttpPost]
+    public ActionResult Edit(Item item, int CategoryId)
     {
+      if (CategoryId != 0)
+      {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+      }
       _db.Entry(item).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult AddCategory(int id)
+{
+    var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    return View(thisItem);
+}
+[HttpPost]
+public ActionResult AddCategory(Item item, int CategoryId)
+{
+    if (CategoryId != 0)
+    {
+    _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+    }
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+}
+
 
     public ActionResult Delete(int id)
-    {
-      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-      return View(thisItem);
-    }
+{
+    var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    return View(thisItem);
+}
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-      _db.Items.Remove(thisItem);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+[HttpPost, ActionName("Delete")]
+public ActionResult DeleteConfirmed(int id)
+{
+    var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    _db.Items.Remove(thisItem);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+}
+[HttpPost]
+public ActionResult DeleteCategory(int joinId)
+{
+    var joinEntry = _db.CategoryItem.FirstOrDefault(entry => entry.CategoryItemId == joinId);
+    _db.CategoryItem.Remove(joinEntry);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+}
+
   }
 }
--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -2,16 +2,15 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-    public class Category
+  public class Category
     {
         public Category()
-                {
-                    this.Items = new HashSet<Item>();
-                }
-        
-                public int CategoryId { get; set; }
-                public string Name { get; set; }
-                public virtual ICollection<Item> Items { get; set; }
-            }
-        
+        {
+            this.Items = new HashSet<CategoryItem>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<CategoryItem> Items { get; set; }
     }
+}
